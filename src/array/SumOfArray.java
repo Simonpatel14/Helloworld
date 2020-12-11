772a4390c55c8efa768727a3a3c5112fd795f11d
Sejal <Simon.patel1401@gmail.com>
2020-12-11 19:33:57 +0000

    new commit

---
+++ src/array/SumOfArray.java
@@ -0,0 +1,23 @@
+package array;
+
+public class SumOfArray {
+    public static void main (String[] args){
+      /*  int sum = 0;
+        //Declaring array value
+        int[] myarray = {10,20,30,40,50};
+        for (int i=0;i< myarray.length; i++);
+        {
+            sum = sum + myarray[i];
+        }
+            System.out.println(sum);
+        }*/
+
+        //Declaring array values
+        int[] myarray = {11, 22, 13, 34, 25};
+        for (int i = 0; i < myarray.length; i++) {
+            System.out.println(i);}
+
+
+
+    }
+}
